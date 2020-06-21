e0cb161d8b7730045cbfcedf636c00c67eabea8f
Kelly Blackwell <kellylenorablackwell@gmail.com>
2020-06-21 04:58:02 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -12,13 +12,13 @@ end
 
 
 def not_safe?(speed)
-  if speed > 60
+  if speed > 60 ? TRUE : FALSE
     return TRUE
   end
   if speed < 40
     return TRUE
   end
-  if speed > 40 || speed < 60
+  if speed > 40 || speed < 60 
     return FALSE
   end
 end
